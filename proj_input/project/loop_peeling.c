5fd53e592b318bd92afef132429e3b359a10ea09
Aditya Tayade <atayade@umich.com>
2012-12-08 20:13:08 -0500

    loop peeling example

--- proj_input/project/loop_peeling.c
+++ proj_input/project/loop_peeling.c
@@ -2,9 +2,15 @@
 
 int main()
 {
-    int a = 0;
+    int a = 0, b = 1;
     for(int i = 0; i<=10;i++) {
         a = a+i;
+        if(a%2 == 0) {
+            a = a+3;    
+        } else {
+            a = a+5;
+        }
     }
+    printf("%d",a);
     return a;
 }
